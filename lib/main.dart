fcb49af82ca7d8490a1f05eb04b91cd77d2dbf21
John Diamond <jdiamond@stalag13.net>
2024-11-14 14:15:30 -0600

    REFACTOR - removed boilerplate comments from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,10 @@ Future<void> main() async {
   await runFermiApp(appWidget: const App());
 }
 
-// This widget is the root of the application.
-
 class App extends StatelessWidget {
   const App({super.key});
 
   static const _title = 'Plotting Widgets Demo';
-
-  // At the very least, the root widget should be `StandardApp`, which
-  // provides a common look and feel. In this case, we wrap the
-  // `StandardApp` with an `ACSysProvider`, which allows our app to
-  // access the control system.
-  //
-  // By wrapping the `StandardApp` it also allows the `AppBar` to pull info
-  // from the control system (not used in this demo, but it's good to keep in
-  // mind.)
 
   @override
   Widget build(BuildContext context) => ACSysProvider(
@@ -30,21 +19,11 @@ class App extends StatelessWidget {
           body: _BaseWidget()));
 }
 
-// This is the body of the application. Typically the body will be implemented
-// by widgets defined in other modules.
-//
-// You may wonder why, with such a simple example, this wasn't placed in the
-// `App` widget. It's because, in `App's` `build` method, the 'context' points
-// to where in the widget tree `App` will be mounted. But it hasn't yet been
-// added to the tree so, when we call `ACSys.api(context)`, it won't find any
-// `ACSysProvider` in the tree.
-//
-// Making a new widget means, when the `build()` method is called, the widgets
-// higher in the tree will have been added and `ACSys.api(context)` will
-// succeed.
-
 class _BaseWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) =>
-      const Center(child: PlotWidget(plotChannels: ["PLOT TEST PARABOLA"]));
+      Column(children: [_buildWaveformPlot()]);
+
+  Widget _buildWaveformPlot() =>
+      const Expanded(child: PlotWidget(plotChannels: ["PLOT TEST PARABOLA"]));
 }
