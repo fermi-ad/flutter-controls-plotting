7fb4612bbb5b15068866ae7264b71667eafabe7e
John Diamond <jdiamond@stalag13.net>
2024-11-20 07:24:21 -0600

    REFACTOR - added card titles to each PlotCardWidget

--- lib/main.dart
+++ lib/main.dart
@@ -33,15 +33,17 @@ class _BaseWidget extends StatelessWidget {
             _buildMultipleWaveformPlot()
           ]));
 
-  Widget _buildEmptyPlot() =>
-      const Expanded(child: PlotCardWidget(plot: PlotWidget()));
+  Widget _buildEmptyPlot() => const Expanded(
+      child: PlotCardWidget(title: "Empty Plot", plot: PlotWidget()));
 
   Widget _buildWaveformPlot() => const Expanded(
       child: PlotCardWidget(
+          title: "Single Waveform Plot",
           plot: PlotWidget(plotChannels: {"PLOT TEST PARABOLA"})));
 
   Widget _buildMultipleWaveformPlot() => const Expanded(
       child: PlotCardWidget(
+          title: "Multiple Waveforms Plot",
           plot: PlotWidget(
               plotChannels: {"PLOT TEST RAND RAMP", "PLOT TEST NORMAL"})));
 }
