254af425935da88430418a920a76972b99060ed3
John Diamond <jdiamond@stalag13.net>
2024-11-20 07:03:07 -0600

    RED - failing widget test for displaying a title in a PlotCardWidget

--- lib/widgets/plot_card_widget.dart
+++ lib/widgets/plot_card_widget.dart
@@ -3,12 +3,16 @@ import 'package:flutter/material.dart';
 class PlotCardWidget extends StatelessWidget {
   final Widget child;
 
-  const PlotCardWidget({super.key, required this.child});
+  final String? title;
+
+  const PlotCardWidget({super.key, required this.child, this.title});
 
   @override
   Widget build(BuildContext context) => Card(
       color: Theme.of(context).colorScheme.primaryContainer,
-      child: SizedBox(
-          height: 400,
-          child: Padding(padding: const EdgeInsets.all(10), child: child)));
+      child: Column(children: [
+        SizedBox(
+            height: 400,
+            child: Padding(padding: const EdgeInsets.all(10), child: child))
+      ]));
 }
--- test/widget/plot_card_widget_test.dart
+++ test/widget/plot_card_widget_test.dart
@@ -1,8 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_controls_core/flutter_controls_core.dart';
+import 'package:flutter_controls_plotting/service/fake_acsys_service.dart';
+import 'package:flutter_controls_plotting/service/plot_daq_service.dart';
+import 'package:flutter_controls_plotting/widgets/plot_card_widget.dart';
+import 'package:flutter_controls_plotting/widgets/plot_widget.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group("PlotCardWidget widget tests", () {
     testWidgets("Provide title, title is displayed with plot",
-        (WidgetTester tester) async {});
+        (WidgetTester tester) async {
+      // Given a PlotCard titled with "Empty Plot"
+      const title = "Empty Plot";
+
+      // When I build the PlotCard
+      tester.pumpWidget(Scaffold(
+          body: ACSysProvider(
+              service: FakeACSysService(),
+              child: const PlotCardWidget(
+                  title: title,
+                  child: PlotWidget(
+                      plotChannels: {}, daqService: StandardPlotDAQ())))));
+
+      // Then the title is displayed inside the card
+      expect(
+          find.ancestor(
+              of: find.text(title),
+              matching: find.byType(PlotCardWidget).first),
+          findsOneWidget);
+    });
   });
 }
