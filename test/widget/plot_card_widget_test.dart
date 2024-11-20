7c76a0f45207526b57a038ff970c1d4395300af3
John Diamond <jdiamond@stalag13.net>
2024-11-20 07:16:28 -0600

    RED - failing widget test for displaying a PlotCard with no title

--- test/widget/plot_card_widget_test.dart
+++ test/widget/plot_card_widget_test.dart
@@ -8,6 +8,25 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group("PlotCardWidget widget tests", () {
+    testWidgets("No title, display plot with no title on card",
+        (WidgetTester tester) async {
+      // Given nothing
+      // When I build the PlotCard with no title parameter
+      await tester.pumpWidget(_buildPlotCard(const PlotCardWidget(
+          child: PlotWidget(plotChannels: {}, daqService: StandardPlotDAQ()))));
+
+      // Then the title is displayed inside the card
+      expect(
+          find.descendant(
+              of: find.byType(Card),
+              matching: find.descendant(
+                  of: find.byType(Column),
+                  matching: find.text("Empty Plot"),
+                  matchRoot: true),
+              matchRoot: true),
+          findsNothing);
+    });
+
     testWidgets("Provide title, title is displayed with plot",
         (WidgetTester tester) async {
       // Given a PlotCard titled with "Empty Plot"
