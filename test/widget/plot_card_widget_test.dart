a69a938a56a8b4abd5c0142d4a5b3a74d24b1c0e
John Diamond <jdiamond@stalag13.net>
2024-11-20 06:56:13 -0600

    GREEN - created a test script for PlotCardWidget widget tests

---
+++ test/widget/plot_card_widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group("PlotCardWidget widget tests", () {
+    testWidgets("Provide title, title is displayed with plot",
+        (WidgetTester tester) async {});
+  });
+}
