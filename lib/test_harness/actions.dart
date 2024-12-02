07eca58694f8ed0977b9108c9cdbbc13ebefa1f6
John Diamond <jdiamond@stalag13.net>
2024-12-02 12:14:38 -0600

    REFACTOR - made the implementation of waitForPlotDataToLoad independent of the plot widget implementation

--- lib/test_harness/actions.dart
+++ lib/test_harness/actions.dart
@@ -1,7 +1,26 @@
 import 'dart:async';
 
-import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_controls_plotting/widgets/plot_widget.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+Future<void> pumpUntilGone(
+  WidgetTester tester,
+  Finder finder, {
+  Duration timeout = const Duration(seconds: 3),
+}) async {
+  bool timerDone = false;
+  final timer = Timer(timeout, () => timerDone = true);
+  while (timerDone != true) {
+    await tester.pumpAndSettle();
+
+    final found = tester.any(finder);
+    if (!found) {
+      timerDone = true;
+    }
+  }
+  timer.cancel();
+}
 
 Future<void> pumpUntilFound(
   WidgetTester tester,
@@ -21,5 +40,10 @@ Future<void> pumpUntilFound(
   timer.cancel();
 }
 
-Future<void> waitForPlotDataToLoad(WidgetTester tester) async =>
-    pumpUntilFound(tester, find.byType(LineChart));
+Future<void> waitForPlotDataToLoad(WidgetTester tester) async {
+  await pumpUntilGone(
+      tester,
+      find.descendant(
+          of: find.byType(PlotWidget),
+          matching: find.byType(LinearProgressIndicator)));
+}
