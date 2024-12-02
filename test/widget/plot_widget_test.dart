32ab6673c7c330f72369174fbaeab4b0636b9142
John Diamond <jdiamond@stalag13.net>
2024-12-02 11:12:57 -0600

    GREEN - added the parabola test to the Graphic widget test group

--- test/widget/plot_widget_test.dart
+++ test/widget/plot_widget_test.dart
@@ -404,6 +404,32 @@ void main() {
       assertPlotXAxisLimits(tester, min: -250, max: 499);
       assertPlotYAxisLimits(tester, min: -1, max: 5);
     });
+
+    testWidgets(
+        "Plot PLOT TEST PARABOLA, get a parabola starting at X=-50 and ending at X=50",
+        (WidgetTester tester) async {
+      // Given a channel list containing "PLOT TEST PARABOLA"
+      final channelList = {"PLOT TEST PARABOLA": ChannelSetting()};
+
+      // When I build the PlotWidget
+      await tester.pumpWidget(
+          _buildPlotWidget(channelList, impl: PlotImplementation.graphic));
+      await waitForPlotDataToLoad(tester);
+
+      // Then the plot contains a parabola with 500 points starting at X=-250
+      assertPlotContainsParabola(tester,
+          numberOfPoints: 501, startingAtX: -250);
+
+      // ... and the Y-axis is labeled
+      assertPlotYAxisTitles(tester,
+          titles: ["PLOT TEST PARABOLA"], units: ["V"]);
+
+      // ... and the limits for the X-axis are -250 to 250
+      assertPlotXAxisLimits(tester, min: -250.0, max: 250.0);
+
+      // ... and the X-axis is labeled...
+      assertPlotXAxisTitle(tester, title: "Index");
+    });
   });
 
   group("PlotWidget (implementation = Fermi) widget tests", () {});
