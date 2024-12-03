439712b8f17ca122ee84da512a14c62bd771a4b0
John Diamond <jdiamond@stalag13.net>
2024-12-03 09:42:04 -0600

    REFACTOR - extracted _buildChart()

--- lib/widgets/graphic_plot_widget_adapter.dart
+++ lib/widgets/graphic_plot_widget_adapter.dart
@@ -7,54 +7,7 @@ class GraphicPlotWidgetAdapter extends PlotWidgetAdapter {
   Widget buildPlot() {
     return LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) =>
-            _buildAxisTitles(
-                constraints: constraints,
-                child: Chart(
-                  data: _data,
-                  variables: {
-                    'Index': Variable(
-                        accessor: (Map datum) => datum['Index'] as double,
-                        scale: LinearScale(title: "Index")),
-                    'Value': Variable(
-                        accessor: (Map datum) => datum['Value'] as double,
-                        scale: LinearScale()),
-                    'Channel': Variable(
-                      accessor: (Map datum) => datum['Channel'] as String,
-                    ),
-                  },
-                  marks: [
-                    LineMark(
-                      position:
-                          Varset('Index') * Varset('Value') / Varset('Channel'),
-                      color: (widget.plotChannels.isEmpty ||
-                              widget.plotChannels.length == 1)
-                          ? ColorEncode(value: _channelColorList.first)
-                          : ColorEncode(
-                              variable: "Channel", values: _channelColorList),
-                    ),
-                  ],
-                  coord: RectCoord(),
-                  axes: [
-                    Defaults.horizontalAxis,
-                    Defaults.verticalAxis,
-                  ],
-                  selections: {
-                    'touchMove': PointSelection(
-                      on: {
-                        GestureType.scaleUpdate,
-                        GestureType.tapDown,
-                        GestureType.longPressMoveUpdate
-                      },
-                      dim: Dim.x,
-                    )
-                  },
-                  tooltip: TooltipGuide(
-                    followPointer: [true, true],
-                    align: Alignment.topLeft,
-                    variables: ['Channel', 'Value'],
-                  ),
-                  crosshair: CrosshairGuide(followPointer: [false, true]),
-                )));
+            _buildAxisTitles(constraints: constraints, child: _buildChart()));
   }
 
   Widget _buildAxisTitles(
@@ -74,6 +27,51 @@ class GraphicPlotWidgetAdapter extends PlotWidgetAdapter {
     children.add(Expanded(child: child));
     return onTop ? Column(children: children) : Row(children: children);
   }
+
+  Widget _buildChart() => Chart(
+        data: _data,
+        variables: {
+          'Index': Variable(
+              accessor: (Map datum) => datum['Index'] as double,
+              scale: LinearScale(title: "Index")),
+          'Value': Variable(
+              accessor: (Map datum) => datum['Value'] as double,
+              scale: LinearScale()),
+          'Channel': Variable(
+            accessor: (Map datum) => datum['Channel'] as String,
+          ),
+        },
+        marks: [
+          LineMark(
+            position: Varset('Index') * Varset('Value') / Varset('Channel'),
+            color: (widget.plotChannels.isEmpty ||
+                    widget.plotChannels.length == 1)
+                ? ColorEncode(value: _channelColorList.first)
+                : ColorEncode(variable: "Channel", values: _channelColorList),
+          ),
+        ],
+        coord: RectCoord(),
+        axes: [
+          Defaults.horizontalAxis,
+          Defaults.verticalAxis,
+        ],
+        selections: {
+          'touchMove': PointSelection(
+            on: {
+              GestureType.scaleUpdate,
+              GestureType.tapDown,
+              GestureType.longPressMoveUpdate
+            },
+            dim: Dim.x,
+          )
+        },
+        tooltip: TooltipGuide(
+          followPointer: [true, true],
+          align: Alignment.topLeft,
+          variables: ['Channel', 'Value'],
+        ),
+        crosshair: CrosshairGuide(followPointer: [false, true]),
+      );
 
   List<Map<String, dynamic>> get _data {
     List<Map<String, dynamic>> data = [];
