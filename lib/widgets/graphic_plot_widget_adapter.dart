e513446386aefa180ae29096ae54dd49e65d321f
John Diamond <jdiamond@stalag13.net>
2024-12-03 07:18:00 -0600

    REFACTOR - added axis titles to Graphic Chart

--- lib/widgets/graphic_plot_widget_adapter.dart
+++ lib/widgets/graphic_plot_widget_adapter.dart
@@ -5,50 +5,74 @@ class GraphicPlotWidgetAdapter extends PlotWidgetAdapter {
 
   @override
   Widget buildPlot() {
-    return Chart(
-      data: _data,
-      variables: {
-        'Index': Variable(
-            accessor: (Map datum) => datum['Index'] as double,
-            scale: LinearScale()),
-        'Value': Variable(
-            accessor: (Map datum) => datum['Value'] as double,
-            scale: LinearScale()),
-        'Channel': Variable(
-          accessor: (Map datum) => datum['Channel'] as String,
-        ),
-      },
-      marks: [
-        LineMark(
-          position: Varset('Index') * Varset('Value') / Varset('Channel'),
-          color:
-              (widget.plotChannels.isEmpty || widget.plotChannels.length == 1)
-                  ? ColorEncode(value: _channelColorList.first)
-                  : ColorEncode(variable: "Channel", values: _channelColorList),
-        ),
-      ],
-      coord: RectCoord(),
-      axes: [
-        Defaults.horizontalAxis,
-        Defaults.verticalAxis,
-      ],
-      selections: {
-        'touchMove': PointSelection(
-          on: {
-            GestureType.scaleUpdate,
-            GestureType.tapDown,
-            GestureType.longPressMoveUpdate
-          },
-          dim: Dim.x,
-        )
-      },
-      tooltip: TooltipGuide(
-        followPointer: [false, true],
-        align: Alignment.topLeft,
-        offset: const Offset(-20, -20),
-      ),
-      crosshair: CrosshairGuide(followPointer: [false, true]),
-    );
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) =>
+            _buildAxisTitles(
+                constraints: constraints,
+                child: Chart(
+                  data: _data,
+                  variables: {
+                    'Index': Variable(
+                        accessor: (Map datum) => datum['Index'] as double,
+                        scale: LinearScale(title: "Index")),
+                    'Value': Variable(
+                        accessor: (Map datum) => datum['Value'] as double,
+                        scale: LinearScale()),
+                    'Channel': Variable(
+                      accessor: (Map datum) => datum['Channel'] as String,
+                    ),
+                  },
+                  marks: [
+                    LineMark(
+                      position:
+                          Varset('Index') * Varset('Value') / Varset('Channel'),
+                      color: (widget.plotChannels.isEmpty ||
+                              widget.plotChannels.length == 1)
+                          ? ColorEncode(value: _channelColorList.first)
+                          : ColorEncode(
+                              variable: "Channel", values: _channelColorList),
+                    ),
+                  ],
+                  coord: RectCoord(),
+                  axes: [
+                    Defaults.horizontalAxis,
+                    Defaults.verticalAxis,
+                  ],
+                  selections: {
+                    'touchMove': PointSelection(
+                      on: {
+                        GestureType.scaleUpdate,
+                        GestureType.tapDown,
+                        GestureType.longPressMoveUpdate
+                      },
+                      dim: Dim.x,
+                    )
+                  },
+                  tooltip: TooltipGuide(
+                    followPointer: [true, true],
+                    align: Alignment.topLeft,
+                    variables: ['Channel', 'Value'],
+                  ),
+                  crosshair: CrosshairGuide(followPointer: [false, true]),
+                )));
+  }
+
+  Widget _buildAxisTitles(
+      {required BoxConstraints constraints, required Widget child}) {
+    final onTop = constraints.maxWidth < 600;
+
+    List<Widget> children = <Widget>[];
+    if (plotReply != null) {
+      for (final channel in plotReply!.data) {
+        final label = Text("${channel.name} (${channel.units})",
+            style: TextStyle(color: lineColorForChannel(channel.name)));
+        children
+            .add(onTop ? label : RotatedBox(quarterTurns: -1, child: label));
+      }
+    }
+
+    children.add(Expanded(child: child));
+    return onTop ? Column(children: children) : Row(children: children);
   }
 
   List<Map<String, dynamic>> get _data {
