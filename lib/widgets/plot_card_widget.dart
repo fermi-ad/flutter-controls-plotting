98bc0ac52e5883e0b890dd4af4f59c361c91e4ac
John Diamond <jdiamond@stalag13.net>
2024-12-17 13:53:58 -0600

    REFACTOR - replaced the SizedBox with an Expanded

--- lib/widgets/plot_card_widget.dart
+++ lib/widgets/plot_card_widget.dart
@@ -17,8 +17,6 @@ class PlotCardWidget extends StatelessWidget {
             : Padding(
                 padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
                 child: Text(title!)),
-        SizedBox(
-            height: 400,
-            child: Padding(padding: const EdgeInsets.all(10), child: plot))
+        Expanded(child: Padding(padding: const EdgeInsets.all(10), child: plot))
       ]));
 }
