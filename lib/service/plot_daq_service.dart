a3d3b3417a74657c728b55dd9d3ecd0e381018b3
Dariusz Jarosz <djarosz@anl.gov>
2025-03-07 12:57:06 -0600

    provide a t in the plot daq service. x becomes 'eventX whenever applicable.

--- lib/service/plot_daq_service.dart
+++ lib/service/plot_daq_service.dart
@@ -153,27 +153,35 @@ class StandardPlotDAQ implements PlotDAQService {
     List<PlotChannelData> internalDaqData = [];
     var xAxisUnits = 'Index';
 
+    DateTime now = DateTime.now();
+    var currentEpochTime = now.millisecondsSinceEpoch / 1000;
+
     for (var forChannel in forChannels) {
       List<PlotPoint>? data;
       if (forChannel == GenPlots.constant.name) {
-        data = List.generate(500, (i) => PlotPoint(x: i.toDouble(), y: 5.0));
+        data = List.generate(500,
+            (i) => PlotPoint(x: i.toDouble(), y: 5.0, t: currentEpochTime));
       } else if (forChannel == GenPlots.randConst.name) {
         var rand = Random();
         var constant = rand.nextInt(25);
         data = List.generate(
-            500, (i) => PlotPoint(x: i.toDouble(), y: constant.toDouble()));
+            500,
+            (i) => PlotPoint(
+                x: i.toDouble(), y: constant.toDouble(), t: currentEpochTime));
       } else if (forChannel == GenPlots.ramp.name) {
         data = List.generate(
-            500, (i) => PlotPoint(x: i.toDouble(), y: i.toDouble()));
+            500,
+            (i) => PlotPoint(
+                x: i.toDouble(), y: i.toDouble(), t: currentEpochTime));
       } else if (forChannel == GenPlots.randRamp.name) {
         var rand = Random();
         data = List.generate(
             500,
             (i) => PlotPoint(
-                x: i.toDouble(), y: i.toDouble() + (rand.nextInt(50) - 25)));
+                x: i.toDouble(),
+                y: i.toDouble() + (rand.nextInt(50) - 25),
+                t: currentEpochTime));
       } else if (forChannel == GenPlots.scalarRamp.name) {
-        DateTime now = DateTime.now();
-        var currentEpochTime = now.millisecondsSinceEpoch / 1000;
         lastScalarRampEpochTime ??= currentEpochTime;
         if (scalarRampCountLimit != null &&
             scalarRampCount >= scalarRampCountLimit!) {
@@ -183,9 +191,9 @@ class StandardPlotDAQ implements PlotDAQService {
           var difference = currentEpochTime - lastScalarRampEpochTime!;
           xAxisUnits = 'Time';
 
-          data = [
-            PlotPoint(x: currentEpochTime, y: difference, eventX: eventX)
-          ];
+          var x = eventX ?? currentEpochTime;
+
+          data = [PlotPoint(x: x, y: difference, t: currentEpochTime)];
         }
       } else if (forChannel == GenPlots.scalarRandRamp.name) {
         var rand = Random();
@@ -198,23 +206,30 @@ class StandardPlotDAQ implements PlotDAQService {
 
         xAxisUnits = 'Time';
 
-        data = [PlotPoint(x: currentEpochTime, y: value, eventX: eventX)];
+        var x = eventX ?? currentEpochTime;
+
+        data = [PlotPoint(x: x, y: value, t: currentEpochTime)];
       } else if (forChannel == GenPlots.parabola.name) {
         data = List.generate(
             501,
             (i) => PlotPoint(
-                x: (i - 250.0).toDouble(), y: pow(i - 250, 2).toDouble()));
+                x: (i - 250.0).toDouble(),
+                y: pow(i - 250, 2).toDouble(),
+                t: currentEpochTime));
       } else if (forChannel == GenPlots.parabola64k.name) {
         data = List.generate(
             65535,
             (i) => PlotPoint(
-                x: (i - 32767.0).toDouble(), y: pow(i - 32767, 2).toDouble()));
+                x: (i - 32767.0).toDouble(),
+                y: pow(i - 32767, 2).toDouble(),
+                t: currentEpochTime));
       } else if (forChannel == GenPlots.sine.name) {
         data = List.generate(
             501,
             (i) => PlotPoint(
                 x: (i - 250.0).toDouble(),
-                y: sin((i - 250) / 500 * 6.28).toDouble()));
+                y: sin((i - 250) / 500 * 6.28).toDouble(),
+                t: currentEpochTime));
       } else if (forChannel == GenPlots.normal.name) {
         data = List.generate(
             500,
@@ -222,7 +237,8 @@ class StandardPlotDAQ implements PlotDAQService {
                 x: i.toDouble(),
                 y: (pow(500, 2) / 4) *
                     pow(e, -(pow(i - 250, 2) / (2 * pow(50, 2)))).toDouble() /
-                    (50 * sqrt(2 * pi))));
+                    (50 * sqrt(2 * pi)),
+                t: currentEpochTime));
       }
 
       if (data != null) {
