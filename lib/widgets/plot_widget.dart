728662a7fe506b9d1339d726b7f9bd31b1e254ca
Dariusz Jarosz <djarosz@anl.gov>
2025-04-24 09:04:14 -0500

    Move the verify if plot reply is valid to the filter points function. Should not stop execution of other builds of stream builder.

--- lib/widgets/plot_widget.dart
+++ lib/widgets/plot_widget.dart
@@ -342,12 +342,6 @@ class PlotState extends State<PlotWidget> {
   }
 
   void _receiveData(PlotReply plotReply) {
-    // Verify if plotReply still needs to be processed.
-    // Streambuilder by design seems to resend last plot reply on each update.
-    if (!widget.plotData.isPlotReplyValid(plotReply)) {
-      return;
-    }
-
     if (widget.isPaused) {
       if (lastReply != null) {
         _adapter.plotReply = lastReply;
@@ -413,6 +407,12 @@ class PlotState extends State<PlotWidget> {
 
   void _filterPoints() {
     if (_adapter.plotReply == null) {
+      return;
+    }
+
+    // Verify if plotReply still needs to be processed.
+    // Streambuilder by design seems to resend last plot reply on each update.
+    if (!widget.plotData.isPlotReplyValid(_adapter.plotReply!)) {
       return;
     }
 
