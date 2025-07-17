e5d52b1fa799e55340d364547af4a548217023e9
John Diamond <jdiamond@stalag13.net>
2025-07-17 13:48:49 -0500

    REFACTOR - removed print statement

--- lib/widgets/plot_y_axis_label_widget.dart
+++ lib/widgets/plot_y_axis_label_widget.dart
@@ -11,8 +11,5 @@ class PlotYAxisLabelWidget extends StatelessWidget {
       : value = (normalizedValue * ((max ?? 1) - (min ?? 0))) + (min ?? 0);
 
   @override
-  Widget build(BuildContext context) {
-    print("$min $max $normalizedValue $value");
-    return Text(value.toStringAsFixed(2));
-  }
+  Widget build(BuildContext context) => Text(value.toStringAsFixed(2));
 }
