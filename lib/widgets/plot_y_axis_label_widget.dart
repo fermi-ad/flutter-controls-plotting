d4fd78811622a8e8ccc409317db8672a48571638
John Diamond <jdiamond@stalag13.net>
2025-07-17 11:47:14 -0500

    GREEN - added parentheses that scale the normalizedValue to a value correctly

--- lib/widgets/plot_y_axis_label_widget.dart
+++ lib/widgets/plot_y_axis_label_widget.dart
@@ -8,7 +8,7 @@ class PlotYAxisLabelWidget extends StatelessWidget {
 
   const PlotYAxisLabelWidget(
       {super.key, required this.normalizedValue, this.min, this.max})
-      : value = (normalizedValue * (max ?? 1) - (min ?? 0)) + (min ?? 0);
+      : value = (normalizedValue * ((max ?? 1) - (min ?? 0))) + (min ?? 0);
 
   @override
   Widget build(BuildContext context) {
