9799c71abd901367f2ddba8987ede3467913ea3c
Dariusz Jarosz <djarosz@anl.gov>
2025-09-09 09:23:36 -0500

    Format according to updated standards.

--- test/widget/plot_card_widget_test.dart
+++ test/widget/plot_card_widget_test.dart
@@ -9,51 +9,72 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group("PlotCardWidget widget tests", () {
-    testWidgets("No title, display plot with no title on card",
-        (WidgetTester tester) async {
+    testWidgets("No title, display plot with no title on card", (
+      WidgetTester tester,
+    ) async {
       // Given nothing
       // When I build the PlotCard with no title parameter
-      await tester.pumpWidget(_buildPlotCard(PlotCardWidget(
-          plot: PlotWidget(
+      await tester.pumpWidget(
+        _buildPlotCard(
+          PlotCardWidget(
+            plot: PlotWidget(
               plotData: PlotData(),
               plotChannels: const {},
-              daqService: StandardPlotDAQ()))));
+              daqService: StandardPlotDAQ(),
+            ),
+          ),
+        ),
+      );
 
       // Then the title is displayed inside the card
       expect(
-          find.descendant(
-              of: find.byType(Card),
-              matching: find.descendant(
-                  of: find.byType(Column),
-                  matching: find.text("Empty Plot"),
-                  matchRoot: true),
-              matchRoot: true),
-          findsNothing);
+        find.descendant(
+          of: find.byType(Card),
+          matching: find.descendant(
+            of: find.byType(Column),
+            matching: find.text("Empty Plot"),
+            matchRoot: true,
+          ),
+          matchRoot: true,
+        ),
+        findsNothing,
+      );
     });
 
-    testWidgets("Provide title, title is displayed with plot",
-        (WidgetTester tester) async {
+    testWidgets("Provide title, title is displayed with plot", (
+      WidgetTester tester,
+    ) async {
       // Given a PlotCard titled with "Empty Plot"
       const title = "Empty Plot";
 
       // When I build the PlotCard
-      await tester.pumpWidget(_buildPlotCard(PlotCardWidget(
-          title: title,
-          plot: PlotWidget(
+      await tester.pumpWidget(
+        _buildPlotCard(
+          PlotCardWidget(
+            title: title,
+            plot: PlotWidget(
               plotData: PlotData(),
               plotChannels: const {},
-              daqService: StandardPlotDAQ()))));
+              daqService: StandardPlotDAQ(),
+            ),
+          ),
+        ),
+      );
 
       // Then the title is displayed inside the card
       expect(
-          find.ancestor(
-              of: find.text(title),
-              matching: find.byType(PlotCardWidget).first),
-          findsOneWidget);
+        find.ancestor(
+          of: find.text(title),
+          matching: find.byType(PlotCardWidget).first,
+        ),
+        findsOneWidget,
+      );
     });
   });
 }
 
 Widget _buildPlotCard(PlotCardWidget card) => MaterialApp(
-    home: Scaffold(
-        body: ACSysProvider.factory(service: FakeACSysService())(child: card)));
+  home: Scaffold(
+    body: ACSysProvider.factory(service: FakeACSysService())(child: card),
+  ),
+);
