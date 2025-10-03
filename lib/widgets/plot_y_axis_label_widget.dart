8471779508466c887f25cd12b51ec309349dc1a1
John Diamond <jdiamond@stalag13.net>
2025-10-03 10:55:21 -0500

    REFACTOR - simplified the conditional by using .abs()

--- lib/widgets/plot_y_axis_label_widget.dart
+++ lib/widgets/plot_y_axis_label_widget.dart
@@ -31,9 +31,8 @@ class PlotYAxisLabelWidget extends StatelessWidget {
     return Column(children: labels);
   }
 
-  String _formatValue(double v) => (v >= 10000 || v <= -10000)
-      ? v.toStringAsExponential(2)
-      : v.toStringAsFixed(2);
+  String _formatValue(double v) =>
+      v.abs() >= 10000 ? v.toStringAsExponential(2) : v.toStringAsFixed(2);
 
   double _calculateValue(String channelName) {
     final channel = channels[channelName];
