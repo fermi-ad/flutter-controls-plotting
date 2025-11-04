8b03ce884a13ea3be03b98fee1ee848d0153cb37
xiaol <xiaol@anl.gov>
2025-11-03 19:10:40 -0600

    add parameter sampleOnEvent

--- lib/widgets/plot_widget.dart
+++ lib/widgets/plot_widget.dart
@@ -36,6 +36,8 @@ class PlotWidget extends StatefulWidget {
 
   final int? triggerEvent;
 
+  final int? sampleOnEvent;
+
   final int nAcquisitions;
 
   final bool isShowLabels;
@@ -75,6 +77,7 @@ class PlotWidget extends StatefulWidget {
     this.updateDelay = 0,
     this.nAcquisitions = 0,
     this.triggerEvent,
+    this.sampleOnEvent,
     this.isShowLabels = true,
     this.isPaused = false,
     this.isPersistent = false,
@@ -398,6 +401,7 @@ class PlotState extends State<PlotWidget> {
 
     _channels = Map.from(widget.plotChannels);
     _updateDelay = widget.updateDelay;
+    _sampleOnEvent = widget.sampleOnEvent;
     _triggerEvent = widget.triggerEvent;
     _nAcquisitions = widget.nAcquisitions;
     var apiAcquisitions = _nAcquisitions;
@@ -449,6 +453,7 @@ class PlotState extends State<PlotWidget> {
         startTime: _dataLoggerStartTime,
         endTime: _dataLoggerEndTime,
         chXAxis: widget.chXAxis,
+        sampleOnEvent: _sampleOnEvent,
       );
     }
   }
@@ -599,6 +604,8 @@ class PlotState extends State<PlotWidget> {
   int _updateDelay = 0;
 
   int? _triggerEvent = 0;
+
+  int? _sampleOnEvent;
 
   int _nAcquisitions = 0;
 
