98ef20f24ae33ab36f832313544c1570aee37906
John Diamond <jdiamond@stalag13.net>
2026-04-21 15:13:05 -0500

    Remove _FlSpotExtension indirection to reduce per-spot heap overhead

--- lib/entities/plotting_fl_spot.dart
+++ lib/entities/plotting_fl_spot.dart
@@ -2,18 +2,13 @@ import 'package:fl_chart/fl_chart.dart';
 
 class PlottingFlSpot extends FlSpot {
   final double originalY;
-  final _FlSpotExtension _ext = _FlSpotExtension();
+  double? _normalizedY;
 
   PlottingFlSpot(super.x, super.y) : originalY = y;
 
   @override
   // Returns the normalized Y value if it exists, otherwise returns the original Y value
-  double get y => _ext._normalizedY ?? originalY;
+  double get y => _normalizedY ?? originalY;
 
-  set normalizedY(double? value) => _ext._normalizedY = value;
-}
-
-// Additional class to ensure the flspot is immutable
-class _FlSpotExtension {
-  double? _normalizedY;
+  set normalizedY(double? value) => _normalizedY = value;
 }
